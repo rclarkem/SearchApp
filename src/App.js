75adf272a18afb9c59590a8f9345a76a4ded4582
Reina <r.clarkemitchell@gmail.com>
2020-02-21 17:34:58 -0500

    adding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import SearchBar from './Components/SearchBar'
 import { Container } from 'semantic-ui-react'
 
 export default class App extends Component {
-	onSearchSubmit = term => {
-		console.log(term)
+	onSearchSubmit = async term => {
+		const res = await axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_APP}`,
+			},
+		})
+		console.log(res.data)
 	}
 
 	render() {
